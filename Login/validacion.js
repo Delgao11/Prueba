70401fe05635c3fe3cc5d2bc6ff35eaa46b098cd
JuanLu1411 <94109552+JuanLu1411@users.noreply.github.com>
2022-10-27 18:28:12 +0200

    Add files via upload

---
+++ Login/validacion.js
@@ -0,0 +1,29 @@
+var usuarios=["Juanlu"];
+var contraseñas=["Juanlu"];
+
+function validar(){
+    var usuarioR=document.getElementById("Name").value; 
+    var contraseña1=document.getElementById("Contraseña").value;
+
+    for(var i=0; i<usuarios.length; i++) 
+    {
+        if(usuarioR == usuarios[i])
+        {
+            for(var j=0; j<contraseñas.length; j++) 
+            {
+                if(contraseña1 == contraseñas[j])
+                {
+                    window.location.href = "../Index/index.html";
+                }
+                else
+                {
+                    alert("Contraseña incorrecta.\nPor favor, confirma tu contraseña.");
+                }
+            }
+        }
+        else
+        {
+            alert("¡Aún no te has registrado!");
+        }
+    }
+}
